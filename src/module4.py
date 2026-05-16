5385c114b237b062f422b34b3a8640b18f7a51dc
Maksym Sokolov <mso@anthillagency.com>
2026-05-16 18:42:38 +0200

    style: introduce long line

--- src/module4.py
+++ src/module4.py
@@ -1,2 +1,3 @@
 def fn_4(): return 4
 # refactor marker
+x = 'very long string ' * 60 + 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
